7a0f9b13e56ba154f7b10e8cb1ef9b0dae351eff
tp6xu4jo3 <163460843+tp6xu4jo3@users.noreply.github.com>
2026-09-18 19:59:02 +0800

    chore: configure Drive API key

--- src/config/drive-config.js
+++ src/config/drive-config.js
@@ -1,5 +1,5 @@
 export const DRIVE_CATALOG_CONFIG = Object.freeze({
   folderId: '1_SZt4WOMakWa3aD54W2tYHtdOk44WUUP',
-  apiKey: '',
+  apiKey: 'AIzaSyCE6Sq72uJJ4fEa3YvPuK8GJOl8wYoMg20',
   apiBaseUrl: 'https://www.googleapis.com/drive/v3'
 });
