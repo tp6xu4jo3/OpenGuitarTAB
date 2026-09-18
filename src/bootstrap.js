c5b46090e6cd37b1c4fa26b71c3306d29e563794
tp6xu4jo3 <163460843+tp6xu4jo3@users.noreply.github.com>
2026-09-18 18:26:45 +0800

    feat: expose Drive catalog config

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,14 @@
 import { compactSong, deserializeSong } from './core/song-codec.js';
 import { readLocalLibrary, writeLocalLibrary } from './storage/local-storage.js';
+import { DRIVE_CATALOG_CONFIG } from './config/drive-config.js';
 
-Object.assign(window, { compactSong, deserializeSong, readLocalLibrary, writeLocalLibrary });
+Object.assign(window, {
+  compactSong,
+  deserializeSong,
+  readLocalLibrary,
+  writeLocalLibrary,
+  DRIVE_CATALOG_CONFIG
+});
 
 const scripts = [
   './src/app-runtime.js',
